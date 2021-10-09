41e6f835a47c0317d66fb4412210dded6836a217
comeonandroid <comeonandroid@gmail.com>
2021-10-09 23:54:00 +0300

    add select dropdown

--- elements/AccountSelect.js
+++ elements/AccountSelect.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { useEthers, shortenIfAddress } from '@usedapp/core'
 
@@ -6,17 +6,38 @@ import BoxIcon from './BoxIcon'
 import BoxDropdown from './BoxDropdown'
 
 import styles from 'styles/Widget.module.css'
+import dropdownStyles from 'styles/Dropdown.module.css'
 
 function AccountSelect(props) {
   const { account } = useEthers()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleSelectClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleItemClick = () => {
+    setIsOpen(false)
+  }
 
   return (
-    <div>
-      <div className={styles.accountbox}>
+    <div className={dropdownStyles.wrapper}>
+      <div className={styles.accountbox} onClick={handleSelectClick}>
         <BoxIcon src='/metamask.svg' alt='Metamask logo' />
         <div className={styles.account}>{account ? shortenIfAddress(account) : 'Connect wallet'}</div>
         <BoxDropdown />
       </div>
+    <div className={dropdownStyles.container} style={{ display: isOpen ? 'block' : 'none' }}>
+      <div className={dropdownStyles.item} onClick={handleItemClick}>
+        <p>Test</p>
+      </div>
+      <div className={dropdownStyles.item} onClick={handleItemClick}>
+        <p>Test</p>
+      </div>
+      <div className={dropdownStyles.item} onClick={handleItemClick}>
+        <p>Test</p>
+      </div>
+    </div>
     </div>
   )
 }
