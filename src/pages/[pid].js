ab078f6c2106b1789ffdd8e9afb13b3c7db1dfae
Alex Tkachenko <skyalexx@gmail.com>
2021-10-09 20:19:50 +0300

    Horizontal layout

--- src/pages/[pid].js
+++ src/pages/[pid].js
@@ -3,39 +3,45 @@ import Widget from 'components/Widget'
 import Subscribed from 'components/Subscribed'
 import { useRouter } from 'next/router'
 import { useIsSubscribed, useSubscriptionInfo } from 'src/hooks'
+import styles from 'styles/Product.module.css'
 
 
 const Checkout = () => {
   const router = useRouter()
   const { pid } = router.query
-  // console.log(`query = ${JSON.stringify(router.query)}, pid = ${pid}`)
-
-  var isSubscribed = useIsSubscribed(pid)
-  console.log('isSubscribed =', isSubscribed)
 
   const product = useSubscriptionInfo(pid)
-  const loading = typeof isSubscribed === 'undefined' || !product
-
+  // TODO: doesn't work for string pid; usedapp returns undefined (wrong pid argument type)
+  const loading = typeof product === 'undefined'
   // TODO: check by id/slug/something
-  const productNotFound = product && product.amount == 0
+  const productNotFound = !!product && product.amount == 0
+  console.log('product =', product, 'loading =', loading, 'productNotFound =', productNotFound)
 
-  // TODO: testing subscribed layout
+  var isSubscribed = useIsSubscribed(pid)
+  // TODO: uncomment to test subscribed layout
   // if (typeof isSubscribed !== 'undefined') {
   //   isSubscribed = true
   // }
 
-  return <>
+  return <div className={styles.layout}>
     {loading && <span></span>}
-    {!loading && productNotFound && 
-      <p>Product <span className='highlightSpan'>{pid}</span> not found</p>
+    {!loading && productNotFound &&
+      <p style={{ padding: '40px', fontSize: '20px' }}>Subscription product <span className='highlightSpan'>{pid}</span> not found</p>
     }
-    {!loading && !productNotFound && 
-      (isSubscribed
-        ? <Subscribed pid={pid} product={product} />
-        : <Widget pid={pid} product={product} />
-      )
+    {!loading && !productNotFound &&
+      <>
+        <div className={`${styles.side} ${styles.left}`}>
+          {/* Product & NFT */}
+        </div>
+        <div className={styles.divider} />
+        <div className={`${styles.side} ${styles.right}`}>
+          {isSubscribed
+            ? <Subscribed pid={pid} product={product} />
+            : <Widget pid={pid} product={product} />}
+        </div>
+      </>
     }
-  </>
+  </div>
 }
 
 export default Checkout
