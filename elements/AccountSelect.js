e422f623cf546310970e0d1d450979290962adfe
comeonandroid <comeonandroid@gmail.com>
2021-10-10 00:10:48 +0300

    add on outside click hook

--- elements/AccountSelect.js
+++ elements/AccountSelect.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { useEthers, shortenIfAddress } from '@usedapp/core'
 
@@ -8,9 +8,14 @@ import BoxDropdown from './BoxDropdown'
 import styles from 'styles/Widget.module.css'
 import dropdownStyles from 'styles/Dropdown.module.css'
 
+import { useOnClickOutside } from 'src/utils'
+
 function AccountSelect(props) {
   const { account } = useEthers()
+  const ref = useRef()
   const [isOpen, setIsOpen] = useState(false)
+
+  useOnClickOutside(ref, () => setIsOpen(false))
 
   const handleSelectClick = () => {
     setIsOpen(!isOpen)
@@ -21,7 +26,7 @@ function AccountSelect(props) {
   }
 
   return (
-    <div className={dropdownStyles.wrapper}>
+    <div className={dropdownStyles.wrapper} ref={ref}>
       <div className={styles.accountbox} onClick={handleSelectClick}>
         <BoxIcon src='/metamask.svg' alt='Metamask logo' />
         <div className={styles.account}>{account ? shortenIfAddress(account) : 'Connect wallet'}</div>
