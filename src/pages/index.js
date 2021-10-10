69e7a64489f80e8d594323b3c1adf2732524d1ce
comeonandroid <comeonandroid@gmail.com>
2021-10-11 01:34:44 +0300

    Revert "test main page"
    This reverts commit ef13c71f01414e531957e73d3b01e176882c6d64.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,7 @@
-import Widget from 'components/Widget'
-import Subscribed from 'components/Subscribed'
-import { useRouter } from 'next/router'
-import { useIsSubscribed, useProductInfo } from 'src/hooks'
-import styles from 'styles/Page.module.css'
-import Product from 'components/Product'
-
-
 const Home = () => {
-  const router = useRouter()
-  // const { pid } = router.query
-  const pid = 'art_school'
-
-  const product = useProductInfo(pid)
-  // TODO: doesn't work for string pid; usedapp returns undefined (wrong pid argument type)
-  const loading = typeof product === 'undefined'
-  // TODO: check by id/slug/something
-  const productNotFound = !!product && product.owner__ == 0
-  console.log('product =', product, 'loading =', loading, 'productNotFound =', productNotFound)
-
-  var isSubscribed = useIsSubscribed(pid)
-  // TODO: uncomment to test subscribed layout
-  // if (typeof isSubscribed !== 'undefined') {
-  //   isSubscribed = true
-  // }
-
-  return <div className={styles.layout}>
-    {loading && <span></span>}
-    {!loading && productNotFound &&
-      <p style={{ padding: '40px', fontSize: '20px' }}>Subscription product <span className='highlightSpan'>{pid}</span> not found</p>
-    }
-    {!loading && !productNotFound &&
-      <>
-        <div className={`${styles.product}`}>
-          <Product product={product} />
-        </div>
-        <div className={`${styles.payment}`}>
-          {isSubscribed
-            ? <Subscribed pid={pid} product={product} />
-            : <Widget pid={pid} product={product} />}
-        </div>
-      </>
-    }
-  </div>
+  return (
+    <p style={{ fontSize: '20px'}}>{'Go to checkout.andsub.com/{prodct_id}'}</p>
+  )
 }
 
 export default Home
