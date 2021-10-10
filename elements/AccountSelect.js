a56f5e8f8ce65ae09aa5f111a917b9e094dde494
comeonandroid <comeonandroid@gmail.com>
2021-10-10 21:04:41 +0300

    change account select

--- elements/AccountSelect.js
+++ elements/AccountSelect.js
@@ -37,6 +37,9 @@ function AccountSelect(props) {
           <BoxIcon src='/metamask.svg' alt='Metamask logo' />
           <div className={styles.account}>{ shortenIfAddress(account) }</div>
         </div>
+        <div className={dropdownStyles.item} onClick={handleItemClick} style={{paddingTop: '8px', paddingBottom: '14px'}}>
+          <div className={styles.account}><Icon style={{marginLeft: '-10px', position: 'relative', top: '5px'}} /> Connect another wallet</div>
+        </div>
       </div>
     </div>
   )
@@ -47,4 +50,6 @@ AccountSelect.propTypes = {
 }
 
 export default AccountSelect
+
+const Icon = ({...props}) => <svg width="24" height="24" fill="none" {...props}><path d="M12 3.85A1.15 1.15 0 0010.85 5v5.85H5a1.15 1.15 0 000 2.3h5.85V19a1.15 1.15 0 002.3 0v-5.85H19a1.15 1.15 0 000-2.3h-5.85V5A1.15 1.15 0 0012 3.85z" fill="currentColor"></path></svg>
 
