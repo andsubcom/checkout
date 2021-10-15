0cc35240afa5c0cf1d9b8f5b3037f712294478f7
comeonandroid <comeonandroid@gmail.com>
2021-10-15 18:59:27 +0300

    Change connect button

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styles from 'styles/Index.module.css'
 const Home = () => {
   return (
     <div className={styles.layout}>
-      <Link href='/art_school' passHref>
+      <Link href='/andsub_demo' passHref>
         <button className={styles.link}>Open demo checkout</button>
       </Link>
     </div>
